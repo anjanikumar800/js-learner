2e1ab3acc439c3984d152c08ac368c2d7f3d405b
anjanikumar800 <anjnisharma800@gmail.com>
2025-02-27 12:26:17 +0000

    Pending changes exported from your codespace

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const studentId = 1
+let studentName = "Anjani"
+var studentRoll = "240010150018"
+studentYear = "1"
+let studentSemester;
+
+console.log("studentName")
+console.table([studentId, studentName, studentRoll, studentYear, studentSemester])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as a newer version
+
+let name = "Anjani"
+let age = 19
+let login = true
+let pass = true
+let marks;
+let attendence = null
+
+console.log(typeof "Anjani")
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(login))
+console.log(typeof(pass))
+console.log(typeof(marks))
+console.log(typeof(attendence)) // nul  is object data type
+
+// data type
+
+//string
+//number
+// bigint
+//boolean
+//object
+// undefine
